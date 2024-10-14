d56596a32ece70cf3b200706d24d9da96c9099d3
mellcosta <m3licosta@gmail.com>
2024-10-14 18:33:50 +0530

    Component: Banner ✅

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Banner from './components/Banner/Banner';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Banner />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Banner.css'
+
+function Banner (){
+
+    return(
+        <header className="banner">
+            <img src="/img/banner.png" alt="O primeiro Banner" />
+        </header>
+    )
+}
+
+export default Banner;
